869055eaafe2c967cb6317b1213dc7dd85425336
MasujimaRyohei <info@MasujimaRyohei.jp>
2018-10-03 17:33:46 +0900

    Initial commit

---
+++ UniRx-SlackReporter/Assets/UniRxSlackReporter/Scripts/PostData.cs
@@ -0,0 +1,12 @@
+﻿namespace SlackReport
+{
+    /// <summary>
+    /// Post data.
+    /// </summary>
+    [System.Serializable]
+    public struct PostData
+    {
+        public string username;
+        public string text; // Post body.
+    }
+}
