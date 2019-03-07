907aec6f6772578692d243620a074f6ce64ebcef
mr <info@masujimaryohei.jp>
2019-03-07 18:48:55 +0900

    [Add] Example scene.

---
+++ UniRx-SlackReporter/Assets/Example/SlackReportExample.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UniRx.SlackReport
+{
+    /// <summary>
+    /// Slack report example.
+    /// </summary>
+    public class SlackReportExample : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField inputField;
+        [SerializeField]
+        private Button submitButton;
+
+        private void Start()
+        {
+            inputField.OnEndEditAsObservable()
+                      .TakeUntilDestroy(this)
+                      .ThrottleFirst(TimeSpan.FromSeconds(3.0f))
+                      .Subscribe(_ => SubmitToSlack());
+
+            submitButton.OnClickAsObservable()
+                        .TakeUntilDestroy(this)
+                        .ThrottleFirst(TimeSpan.FromSeconds(3.0f))
+                        .Subscribe(_ => SubmitToSlack());
+        }
+
+        private void SubmitToSlack()
+        {
+            SlackReporter.PostText(inputField.text);
+        }
+    }
+}
